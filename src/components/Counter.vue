e37cdacebc27c1db04a343f5d0c46eff16acae82
AnasMaruf <anasmaruf1101@gmail.com>
2025-04-15 11:29:08 +0700

    State

---
+++ src/components/Counter.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { useCounter } from "../store";
+
+const counterStore = useCounter();
+
+counterStore.$subscribe((mutation, state) => {
+  console.info(mutation);
+  console.info(state);
+});
+
+function handleClick() {
+  counterStore.counter++;
+}
+
+function handleReset() {
+  counterStore.$patch({
+    counter: 0,
+  });
+}
+</script>
+<template>
+  <div>
+    <h1>Counter {{ counterStore.counter }}</h1>
+    <button @click="handleClick">Increment</button>
+    <button @click="handleReset">Reset</button>
+  </div>
+</template>
+<style scoped></style>
